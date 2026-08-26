c9c8ab3926538a45a70f922ce221f11511cf5d37
Sergio Bergê <sergioeaberge@gmail.com>
2026-08-26 14:59:31 -0300

    Adiciona logo e ícones das categorias

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import './detail.css';
+import './brand.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
