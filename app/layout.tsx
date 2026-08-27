925bf35dec6c383a7a7b9c58a686f2b6568cd046
Sergio Bergê <sergioeaberge@gmail.com>
2026-08-27 08:58:36 -0300

    Corrige sobreposicao dos valores nos graficos

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import './detail.css';
 import './brand.css';
+import './chart-layout.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
